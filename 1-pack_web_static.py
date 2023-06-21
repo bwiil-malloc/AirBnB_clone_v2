92befde3e6332d3f4cce1c312b041f4cf13afbb6
Efa-Cb <118351358+Efa-Cb@users.noreply.github.com>
2023-06-21 21:58:46 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
+"""A modue for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
 from fabric.api import local, runs_once
